9e3e77910dbbe091be0bd473c1faac3ed1ffb050
Daniil Chemaev <45203038+soundbanana@users.noreply.github.com>
2024-12-15 22:26:01 +0300

    Initial commit

---
+++ bdui-swift/bdui-swift/bdui_swift.h
@@ -0,0 +1,18 @@
+//
+//  bdui_swift.h
+//  bdui-swift
+//
+//  Created by Danil Chemaev on 15.12.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bdui_swift.
+FOUNDATION_EXPORT double bdui_swiftVersionNumber;
+
+//! Project version string for bdui_swift.
+FOUNDATION_EXPORT const unsigned char bdui_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bdui_swift/PublicHeader.h>
+
+
